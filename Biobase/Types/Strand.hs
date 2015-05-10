c42c0310913c6386c0d66b019f27f2629d011ee1
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2015-05-10 16:06:50 +0200

    added serializable Strand type

---
+++ Biobase/Types/Strand.hs
@@ -0,0 +1,96 @@
+
+-- | Strand information. A newtyped version, complete with serialization,
+-- pattern synonyms, being a @PrimitiveArray@ index type, etc.
+
+module Biobase.Types.Strand where
+
+import Control.DeepSeq
+import Data.Aeson
+import Data.Binary
+import Data.Hashable (Hashable)
+import Data.Serialize (Serialize)
+import Data.Vector.Unboxed.Deriving
+import GHC.Generics
+import Test.QuickCheck
+import Text.Printf
+import Data.Vector.Fusion.Stream.Monadic (flatten, Step(..))
+import Data.Vector.Fusion.Stream.Size
+
+import Data.PrimitiveArray.Index.Class
+
+
+
+newtype Strand = Strand { getStrand :: Int }
+  deriving (Eq,Ord,Generic)
+
+instance Bounded Strand where
+  minBound = P
+  maxBound = M
+
+instance Enum Strand where
+  succ P = M
+  succ M = error "succ M"
+  pred M = P
+  pred P = error "pred P"
+  toEnum i = if i>=0 && i<=1 then Strand i else error $ "toEnum (Strand)" ++ show i
+  fromEnum = getStrand
+
+instance NFData Strand
+
+pattern P = Strand 0
+pattern M = Strand 1
+
+pattern Sense     = P
+pattern AntiSense = M
+
+instance Binary    Strand
+instance Serialize Strand
+instance ToJSON    Strand
+instance FromJSON  Strand
+instance Hashable  Strand
+
+derivingUnbox "Strand"
+  [t| Strand -> Int |]
+  [|  getStrand     |]
+  [|  Strand        |]
+
+instance Index Strand where
+  linearIndex _ _ (Strand z) = z
+  {-# INLINE linearIndex #-}
+  smallestLinearIndex (Strand l) = error "still needed?"
+  {-# INLINE smallestLinearIndex #-}
+  largestLinearIndex (Strand h) = h
+  {-# INLINE largestLinearIndex #-}
+  size (_) (Strand h) = h + 1
+  {-# INLINE size #-}
+  inBounds (_) (Strand h) (Strand x) = 0<=x && x<=h
+  {-# INLINE inBounds #-}
+
+instance IndexStream z => IndexStream (z:.Strand) where
+  streamUp (ls:.Strand lf) (hs:.Strand ht) = flatten mk step Unknown $ streamUp ls hs
+    where mk z = return (z,lf)
+          step (z,k)
+            | k > ht    = return $ Done
+            | otherwise = return $ Yield (z:.Strand k) (z,k+1)
+          {-# Inline [0] mk   #-}
+          {-# Inline [0] step #-}
+  {-# Inline streamUp #-}
+  streamDown (ls:.Strand lf) (hs:.Strand ht) = flatten mk step Unknown $ streamDown ls hs
+    where mk z = return (z,ht)
+          step (z,k)
+            | k < lf    = return $ Done
+            | otherwise = return $ Yield (z:.Strand k) (z,k-1)
+          {-# Inline [0] mk   #-}
+          {-# Inline [0] step #-}
+  {-# Inline streamDown #-}
+
+instance IndexStream Strand
+
+instance Arbitrary Strand where
+  arbitrary = do
+    b <- choose (0,1)
+    return $ Strand b
+  shrink (Strand j)
+    | 0<j = [Strand $ j-1]
+    | otherwise = []
+
