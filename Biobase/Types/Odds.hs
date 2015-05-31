aa95ceb216d08b49691746241cea9c9d15c23080
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2015-06-01 00:42:09 +0200

    added discretized odds module

---
+++ Biobase/Types/Odds.hs
@@ -0,0 +1,38 @@
+
+-- | Discretized log-odds.
+
+module Biobase.Types.Odds where
+
+import Control.DeepSeq (NFData(..))
+import Data.Aeson (FromJSON,ToJSON)
+import Data.Binary (Binary)
+import Data.Serialize (Serialize)
+import Data.Vector.Unboxed.Deriving
+import GHC.Generics (Generic)
+
+
+
+-- | Discretized log-odds.
+--
+-- The BLOSUM matrices, for example, store data in discretized log-odds
+-- form.
+--
+-- TODO Might move up even higher into statistics modules.
+
+newtype DLO = DLO { getDLO :: Int }
+  deriving (Generic,Eq,Ord,Show,Read)
+
+derivingUnbox "DLO"
+  [t| DLO -> Int |]
+  [| getDLO      |]
+  [| DLO         |]
+
+instance Binary    DLO
+instance Serialize DLO
+instance FromJSON  DLO
+instance ToJSON    DLO
+
+instance NFData DLO where
+  rnf (DLO k) = rnf k
+  {-# Inline rnf #-}
+
--- changelog.md
+++ changelog.md
@@ -1,3 +1,8 @@
+0.1.1.0
+-------
+
+- added Biobase.Types.Odds module (from the BiobaseBlast package)
+
 0.1.0.0
 -------
 
