495ff8e10126b90b571b628a082c152ac2757036
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-06-22 00:18:21 +0200

    improved default.nix

--- default.nix
+++ default.nix
@@ -2,24 +2,20 @@ with (import <nixpkgs> {});
 with haskell.lib;
 
 rec {
-  hsPkgs = haskellPackages.extend (packageSourceOverrides {
-    bimaps = ../Lib-bimaps;
-    BiobaseTypes = ./.;
-    DPutils = ../Lib-DPutils;
-    ForestStructures = ../Lib-ForestStructures;
-    OrderedBits = ../Lib-OrderedBits;
-    PrimitiveArray = ../Lib-PrimitiveArray;
-    SciBaseTypes = ../Lib-SciBaseTypes;
-  });
+  hsSrcSet = (lib.foldl' (s: p: s // (import p).hsSrcSet) {} [
+    ../Lib-bimaps
+    ../Lib-ForestStructures
+    ../Lib-PrimitiveArray
+    ../Lib-SciBaseTypes
+  ]) // {BiobaseTypes = ./.;};
+  hsPkgs = haskellPackages.extend (packageSourceOverrides hsSrcSet);
   hsShell = with hsPkgs; shellFor {
     packages = p: [ p.BiobaseTypes ];
     withHoogle = true;
     buildInputs = [
       cabal-install ghc
       bimaps
-      DPutils
       ForestStructures
-      OrderedBits
       PrimitiveArray
       SciBaseTypes
     ];
