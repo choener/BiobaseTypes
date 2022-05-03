19ba14be1c54d3b73b66846b18aa2b6d9f2e1082
Christian Hoener zu Siederdissen <christian.hoener.zu.siederdissen@uni-jena.de>
2022-05-03 16:01:22 +0200

    updated default.nix

--- default.nix
+++ default.nix
@@ -1,31 +1,32 @@
 { mkDerivation, aeson, attoparsec, base, bimaps, binary, bytestring
 , cereal, cereal-text, cereal-vector, containers, data-default
-, deepseq, ForestStructures, hashable, intern, lens, mtl, primitive
-, PrimitiveArray, QuickCheck, SciBaseTypes, stdenv, streaming
-, string-conversions, tasty, tasty-hunit, tasty-quickcheck
-, tasty-th, text, text-binary, utf8-string, vector
-, vector-binary-instances, vector-th-unbox
+, deepseq, DPutils, ForestStructures, hashable, intern, lens, lib
+, mtl, primitive, PrimitiveArray, QuickCheck, SciBaseTypes
+, streaming, string-conversions, tasty, tasty-hunit
+, tasty-quickcheck, tasty-th, text, text-binary, utf8-string
+, vector, vector-binary-instances, vector-th-unbox
 }:
 mkDerivation {
   pname = "BiobaseTypes";
-  version = "0.2.0.1";
+  version = "0.2.1.0";
   src = ./.;
   libraryHaskellDepends = [
     aeson attoparsec base bimaps binary bytestring cereal cereal-text
-    cereal-vector containers data-default deepseq ForestStructures
-    hashable intern lens mtl primitive PrimitiveArray QuickCheck
-    SciBaseTypes streaming string-conversions text text-binary
-    utf8-string vector vector-binary-instances vector-th-unbox
+    cereal-vector containers data-default deepseq DPutils
+    ForestStructures hashable intern lens mtl primitive PrimitiveArray
+    QuickCheck SciBaseTypes streaming string-conversions text
+    text-binary utf8-string vector vector-binary-instances
+    vector-th-unbox
   ];
   testHaskellDepends = [
     aeson attoparsec base bimaps binary bytestring cereal cereal-text
-    cereal-vector containers data-default deepseq ForestStructures
-    hashable intern lens mtl primitive PrimitiveArray QuickCheck
-    SciBaseTypes streaming string-conversions tasty tasty-hunit
-    tasty-quickcheck tasty-th text text-binary utf8-string vector
-    vector-binary-instances vector-th-unbox
+    cereal-vector containers data-default deepseq DPutils
+    ForestStructures hashable intern lens mtl primitive PrimitiveArray
+    QuickCheck SciBaseTypes streaming string-conversions tasty
+    tasty-hunit tasty-quickcheck tasty-th text text-binary utf8-string
+    vector vector-binary-instances vector-th-unbox
   ];
   homepage = "https://github.com/choener/BiobaseTypes";
   description = "Collection of types for bioinformatics";
-  license = stdenv.lib.licenses.bsd3;
+  license = lib.licenses.bsd3;
 }
